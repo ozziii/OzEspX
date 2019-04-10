6d90c46ff678037f0d793cc95876e8249f8f8c98
ozziii <spamozziii@gmail.com>
2019-04-10 10:33:52 +0200

    update

--- lib/setting/default_settings.h
+++ lib/setting/default_settings.h
@@ -15,26 +15,26 @@ struct Parameter
 
 const Parameter Parameters[] PROGMEM = {
     { 1   , "0" ,"init"},
-    { 20  , "TEST" ,"espname" }, 
+    { 20  , "NoName" ,"espname" }, 
     { 20  , "admin" ,"WEBUSER" }, 
     { 20  , "admin" ,"WEBPASS" },
-    { 20  , "ozziii.duckdns.org" ,"MQTTSIP" },
-    { 20  , "11883" ,"MQTTPORT" },
+    { 20  , "" ,"MQTTSIP" },
+    { 20  , "" ,"MQTTPORT" },
     { 20  , "admin" ,"MQTTUSER" },
     { 20  , "admin" ,"MQTTPASS" },
     { 100 , "" ,"ddnsUrl" },
-    { 20  , "lukegeorge" , "WIFI1SSID"},
-    { 50  , "microelettronica"  , "WIFI1PASS"},
-    { 20  , "Alice-62361025"  , "WIFI2SSID" },
-    { 50  , "camillis1991de2000manuelamarialu"  , "WIFI2PASS"},
+    { 20  , "" , "WIFI1SSID"},
+    { 50  , ""  , "WIFI1PASS"},
+    { 20  , ""  , "WIFI2SSID" },
+    { 50  , ""  , "WIFI2PASS"},
     { 20  , "" , "WIFI3SSID"},
     { 50  , "" , "WIFI3PAS"},
     { 16  , "" , "IPADDRESS"},   //IP ADDRESS
-    { 16  , "192.168.0.1" , "GATEWAY" },     //GATEWAY
-    { 16  , "255.255.255.0" , "SUBNETMASK" },   //SUBNET MASK
-    { 16  , "8.8.8.8" , "DNS" },         //DNS
-    { 5   , "80"  , "WEBPORT" },              //Server PORT
-    { 5   , "500" ,"DebouncingInterrupt" }, 
+    { 16  , "" , "GATEWAY" },     //GATEWAY
+    { 16  , "" , "SUBNETMASK" },   //SUBNET MASK
+    { 16  , "" , "DNS" },         //DNS
+    { 5   , ""  , "WEBPORT" },              //Server PORT
+    { 5   , "" ,"DebouncingInterrupt" }, 
     { 25   , "" ,"ITEM0" },
     { 25   , "" ,"ITEM1" },
     { 25   , "" ,"ITEM2" },
